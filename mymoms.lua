8f024ebe8c03d297b12c2fb5358b63385af858f5
hitech <113853895+hitechboi@users.noreply.github.com>
2026-03-08 22:21:13 -0500

    Update mymoms.lua

--- mymoms.lua
+++ mymoms.lua
@@ -63,6 +63,9 @@ local ATTRS = {
     CurrentAmmo = function() return infiniteAmmo and ammoVal end,
     ReloadTime  = function() return instantReload and reloadVal end,
     FireRate    = function(tool)
+        if (shotgunAuto and tool.Name == "Remington 870") or (m9FullAuto and tool.Name == "M9") then
+            return nil
+        end
         if instantFire then return 0.001 end
         if fastFire then
             if arInstantFire and AR_NAMES[tool.Name] then return nil end
@@ -189,6 +192,7 @@ updates:Log({
 	"> Uni - Addded ScrollBar",
 	"> Uni - Fixed Buttons clipping out the menu",
 	"> Uni - user greeting plus fixed minimize menu",
+	"> v1.2 - Full auto's are now independent",
     "> hi :p"
 }, true)
 
